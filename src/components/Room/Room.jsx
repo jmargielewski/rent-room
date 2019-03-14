8b5a3dea9f1ea6eada50e9f54c2002200e8b8699
jmargielewski <kuba.margielewski@gmail.com>
2019-03-14 19:59:27 +0000

    styles for Room description

--- src/components/Room/Room.jsx
+++ src/components/Room/Room.jsx
@@ -1,17 +1,56 @@
 import React from 'react';
-import { Card } from './RoomStyles';
+import {
+  Card,
+  Picture,
+  Separator,
+  Price,
+  Title,
+  Area,
+  Icon,
+  Time,
+} from './RoomStyles';
+import { Row, Col } from '../Layout/Grid';
 
-const Room = ({ room }) => (
-  <Card>
-    {room.rentPerMonth}
-    {room.roomTitle}
-    {room.area}
-    {room.transport.name}
-    {room.transport.type}
-    {room.transport.distance.time}
-    {room.transport.distance.type}
-    roommates: .... photos: ....
-  </Card>
-);
+const Room = ({ room }) => {
+  console.log(room);
+  return (
+    <Card>
+      <Picture src={room.photos[0].cdnUrl} alt={room.roomTitle} />
+      <div>
+        <Price>{room.rentPerMonth}</Price>
+        <Separator />
+        <Title>{room.roomTitle}</Title>
+      </div>
+      <div>
+        <Area>{room.area}</Area>
+        <Separator />
+        <Icon />
+        <Time>{room.transport.distance.time}</Time>
+        <Icon />
+        <Area>{room.transport.name}</Area>
+      </div>
+
+      {/* <Row>
+        <Col xs={2} sm={2} md={2} lg={2}>
+          {room.rentPerMonth}
+        </Col>
+        <Col xs={10} sm={10} md={10} lg={10}>
+          {room.roomTitle}
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={2} sm={2} md={2} lg={2}>
+          {room.area}
+        </Col>
+        <Col xs={10} sm={10} md={10} lg={10}>
+          {room.transport.distance.type && 'ludzik'}
+          {room.transport.distance.time}
+          {room.transport.type}
+          {room.transport.name}
+        </Col>
+      </Row> */}
+    </Card>
+  );
+};
 
 export default Room;
--- src/components/Room/RoomStyles.js
+++ src/components/Room/RoomStyles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Card = styled.div`
   background: #f6f7f7;
@@ -7,4 +7,40 @@ const Card = styled.div`
   box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.15);
 `;
 
-export { Card };
+const Picture = styled.img`
+  object-fit: contain;
+  display: inline-block;
+  width: 100%;
+  border-radius: 3px;
+`;
+
+const Separator = styled.span`
+  width: 1px;
+  height: 15px;
+  display: inline-block;
+  background: #aaa;
+  margin: 0 10px;
+  vertical-align: middle;
+`;
+
+const Price = styled.span`
+  font-weight: 600;
+  font-size: 14px;
+`;
+
+const Title = styled.span`
+  font-weight: 600;
+  font-size: 14px;
+`;
+
+const Area = styled.span`
+  font-size: 12px;
+`;
+
+const Icon = styled.i``;
+
+const Time = styled.time`
+  font-size: 12px;
+`;
+
+export { Card, Picture, Separator, Price, Title, Area, Icon, Time };
